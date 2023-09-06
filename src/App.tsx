d43fa36878385e48523b96477f28a666eb762872
unknown <marlon_tancredo@hotmail.com>
2023-09-06 11:46:11 +0100

    feat: creating project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './App.css'
-import { PropertiesDisplay } from './components/properties-display'
+import "./App.css";
+import { PropertiesDisplay } from "./components/properties-display";
 
 function App() {
   return (
     <>
-      <h1>Welcome to Marvelous Mansions</h1>
-      <h2>A one stop shop to your dream property!</h2>
-      <h3>Current property listings:</h3>
+      <div className="mb-20">
+        <h1 className="text-6xl m-10">Welcome to Marvelous Mansions</h1>
+        <h2 className="text-4xl m-10">
+          A one stop shop to your dream property!
+        </h2>
+        <h3 className="text-2xl">Current property listings:</h3>
+      </div>
       <PropertiesDisplay />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,30 +1,41 @@
 export interface IProperty {
-    property: {
-        location: string; 
-        country: string; 
-        property_type: string; 
-        bedroom_count: number; 
-        bathroom_count: number; 
-        price: string; 
-        image_urls: string[]; 
-        title: string; 
-        description: string;
-    };
+  property: {
+    location: string;
+    country: string;
+    property_type: string;
+    bedroom_count: number;
+    bathroom_count: number;
+    price: string;
+    image_urls: string[];
+    title: string;
+    description: string;
+  };
 }
 
-export const Card: React.FC<IProperty> = ({ property: { title, location, country, property_type, image_urls, description, bedroom_count, bathroom_count, price } }) => {
-
-    return (
-        <>
-            <h4>{title}</h4>
-            <p>{location}</p>
-            <p>{country}</p>
-            <p>{property_type}</p>
-            <p>{description}</p>
-            <p>{bedroom_count}</p>
-            <p>{bathroom_count}</p>
-            <p>{price}</p>
-            <img src={image_urls[0]} />
-        </>
-    )
-}
+export const Card: React.FC<IProperty> = ({
+  property: {
+    title,
+    location,
+    country,
+    property_type,
+    image_urls,
+    description,
+    bedroom_count,
+    bathroom_count,
+    price,
+  },
+}) => {
+  return (
+    <div className="mb-20 text-justify">
+      <h4 className="text-2xl">{title}</h4>
+      <p>Location: {location}</p>
+      <p>Country: {country}</p>
+      <p>Type: {property_type}</p>
+      <p>Description: {description}</p>
+      <p>Bedrooms: {bedroom_count}</p>
+      <p>Bathrooms: {bathroom_count}</p>
+      <p>Price: {price}</p>
+      <img src={image_urls[0]} />
+    </div>
+  );
+};
